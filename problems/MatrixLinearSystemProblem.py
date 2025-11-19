a384adce1983b6ca315255ce3fb49366502aa47e
dchu400 <chudaniel400@gmail.com>
2025-11-19 17:07:53 -0500

    Wrote help command interface, solution algorithm for matsys

--- problems/MatrixLinearSystemProblem.py
+++ problems/MatrixLinearSystemProblem.py
@@ -5,22 +5,29 @@ import math
 class MatrixLinearSystemProblem(Problem):
     def __init__(self, dimension: int = 3):
         self.dimension = dimension
+        self.sols = self._generate_solution()
         super().__init__()
     
     def _get_problem_str(self):
-        result = ""
+        result = "Please solve the following linear system using Gaussian Elimination.\n"
         for i in range(self.dimension):
             result += self._new_equation()
         return result
     
+    def _generate_solution(self):
+        sols = []
+        for i in range(self.dimension):
+            ran = math.floor(random.random() * 7 - 3)
+            sols.append(ran) 
+        return sols
+    
     def _new_equation(self) -> str:
         sum = 0
         result = ""
-        for i in range(self.dimension):
-            chosen = math.floor(random.random() * 7 - 3)
-            coef = math.floor(random.random() * 7 - 3)
-            term = coef * chosen
+        for component in self.sols:
+            coef = math.floor(random.random() * 9 - 4)
+            term = coef * component
             sum += term
-            result += f"{term:03d} "
-        result += f"{sum}\n"
+            result += f"{term:3d} "
+        result += f"| {sum:3d}\n"
         return result
--- problems/ProblemFactory.py
+++ problems/ProblemFactory.py
@@ -9,6 +9,13 @@ class ProblemFactory(object):
     def createProblem(problem_type: str, args: list = None) -> Problem:
         if (problem_type == "bstdraw" or problem_type == "bstproblem"):
             return BSTProblem()
+        
         elif (problem_type == "matsys"):
+            if (args and len(args) == 1):
+                try:
+                    dimension = int(args[0])
+                    return MatrixLinearSystemProblem(dimension if 0 < dimension and dimension <= 5 else 3)
+                except ValueError:
+                    pass
             return MatrixLinearSystemProblem()
         return Problem()
