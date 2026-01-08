1b551870037f310b66263bfda940a082165cf8a2
dchu400 <chudaniel400@gmail.com>
2026-01-08 16:07:28 -0500

    Optimized MathProblemFactory.py to get math_ml at very end of create_problem_output method

--- mathproblems/MathProblemFactory.py
+++ mathproblems/MathProblemFactory.py
@@ -20,32 +20,31 @@ from .DiffEqAutonomous import DiffEqAutonomous
 class MathProblemFactory(object):
     def create_problem_output(command: str, args: list[str]) -> str:
         output: str = "Invalid command!"
+        problem = None
+
         if (command in ["arith"]):
             try:
                 num_probs = int(args[0])
                 if (num_probs < 1 or num_probs > 8):
                     raise ValueError()
                 problem = ArithmeticProblem(num_probs) 
-                output = problem.get_mathml()
             except Exception:
                 output = "Argument must be a single integer between 1 and 8!"
 
         elif (command in ["matmul"]):
             problem = MatrixMultiply()
-            output = problem.get_mathml()
+
         elif (command in ["matsys"]):
             try:
                 dimension: int = int(args[0])
                 if (dimension < 2 or dimension > 5):
                     raise ValueError()
                 problem = MatrixLinearSystem(dimension)
-                output = problem.get_mathml()
             except Exception as e:
                 output = "Argument must be an integer between 2 and 5!"
         
         elif (command in ["psys"]):
             problem = LinearPlanarSystem()
-            output = problem.get_mathml()
 
         elif (command in ["expand"]):
             try:
@@ -53,7 +52,6 @@ class MathProblemFactory(object):
                 if (num_roots < 2 or num_roots > 5):
                     raise ValueError()
                 problem = RootExpansion(num_roots)
-                output = problem.get_mathml()
             except TypeError as te:
                 output = "Must include a single argument that is an integer!"
             except IndexError as ie:
@@ -66,7 +64,6 @@ class MathProblemFactory(object):
                 if (degree < 1 or degree > 5):
                     raise ValueError()
                 problem = DerivativePolynomial(degree)
-                output = problem.get_mathml()
             except Exception:
                 output = "There must be exactly one argument that is an integer between 1 and 5!"
         
@@ -79,38 +76,28 @@ class MathProblemFactory(object):
                         if (degree < 1 or degree > 5):
                             raise ValueError()
                         problem = IntegralPolynomial(degree)
-                        output = problem.get_mathml()
                     except Exception:
                         output = "Integral Polynomial syntax error: There must be exactly one argument that is an integer between 1 and 5!"
                 elif (integralType in ["arctan", "invtan"]):
                     problem = IntegralArctan()
-                    output = problem.get_mathml()
                 elif (integralType in ["arcsin", "invsin"]):
                     problem = IntegralArcsin()
-                    output = problem.get_mathml()
                 elif (integralType in ["sincos"]):
                     problem = IntegralSincos()
-                    output = problem.get_mathml()
                 elif (integralType in ["sectan"]):
                     problem = IntegralSectan()
-                    output = problem.get_mathml()
                 elif (integralType == "trigsub"):
                     problem = IntegralTrigSub()
-                    output = problem.get_mathml()
             else:
                 output = "You must specify the type of integral to generate!  Ex: 'int sincos'"
         elif (command == "pfrac"):
             problem = PartialFraction()
-            output = problem.get_mathml()
         elif (command == "pseries"):
             problem = PSeries()
-            output = problem.get_mathml()
         elif (command == "dct"):
             problem = SeriesDirectComparison()
-            output = problem.get_mathml()
         elif (command == "rationalseries"):
             problem = SeriesRational()
-            output = problem.get_mathml()
         elif (command in ["odeauto"]):
             num_stationary_points: int = 2
             try:
@@ -118,7 +105,6 @@ class MathProblemFactory(object):
                     num_stationary_points: int = int(args[0])
                 if (0 < num_stationary_points and num_stationary_points <= 4):
                     problem = DiffEqAutonomous(num_stationary_points)
-                    output = problem.get_mathml()
                 else:
                     raise ValueError()
             except Exception as e:
@@ -126,6 +112,8 @@ class MathProblemFactory(object):
                 
         elif (command in ["ode2"]):
             problem = DiffEqSecondOrder()
+        
+        if (problem != None):
             output = problem.get_mathml()
 
         return output
